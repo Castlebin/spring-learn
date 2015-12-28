c11d08b2c8d0bc6a9088ff8571428ed4f9d216bf
Castlebin <Castlebin@gmail.com>
2015-12-29 00:40:10 +0800

    Consuming a RESTful Web Service

---
+++ spring.io.guides/gs-consuming-rest/src/main/java/com/heller/hello/Application.java
@@ -0,0 +1,27 @@
+package com.heller.hello;
+
+import com.heller.hello.bean.Quote;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        RestTemplate restTemplate = new RestTemplate();
+
+        Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+        log.info(quote.toString());
+    }
+}
