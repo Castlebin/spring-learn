78f5f119b1014c82332b5c4368fad854862b71ce
Castlebin <Castlebin@gmail.com>
2015-12-29 23:02:35 +0800

    Creating a Batch Service, using spring-boot-starter-batch

---
+++ spring.io.guides/gs-batch-processing/src/main/java/com/heller/hello/bean/Person.java
@@ -0,0 +1,19 @@
+package com.heller.hello.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+
+    private String lastName;
+    private String firstName;
+
+}
