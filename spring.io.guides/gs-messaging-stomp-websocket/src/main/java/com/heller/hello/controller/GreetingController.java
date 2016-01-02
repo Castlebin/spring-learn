38110a1f7a62f78d75acd81cdcd6befd4526b382
Castlebin <Castlebin@gmail.com>
2016-01-03 01:42:18 +0800

    Using WebSocket to build an interactive web application

---
+++ spring.io.guides/gs-messaging-stomp-websocket/src/main/java/com/heller/hello/controller/GreetingController.java
@@ -0,0 +1,20 @@
+package com.heller.hello.controller;
+
+import com.heller.hello.vo.Greeting;
+import com.heller.hello.vo.HelloMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class GreetingController {
+
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(3000); // simulated delay
+        return new Greeting("Hello, " + message.getName() + "!");
+    }
+
+}
